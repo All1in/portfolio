7759323e39a502d59f6d3e979f1b39bcc842e750
All1in <bm3023068@gmail.com>
2023-03-04 15:32:35 +0200

    some changes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
+import ReorderIcon from '@material-ui/icons/Reorder'
+import './Navbar.css'
 
 const Navbar = () => {
     return (
